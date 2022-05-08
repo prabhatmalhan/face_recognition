c6e0c52805691ad22be1a860abdb39b243f8c68c
Prabhat Malhan <prabhat.malhan7@gmail.com>
2022-05-08 09:13:52 +0530

    configuration controls added

---
+++ Data Collection/editConfig.py
@@ -0,0 +1,23 @@
+import json
+
+
+class EditConfig:
+    def __init__(self, temp=''):
+        self.__filepath = temp
+
+    def readConfig(self):
+        try:
+            with open(self.__filepath) as f:
+                return json.load(f)
+        except:
+            return None
+
+    def addSample(self, val=''):
+        try:
+            jsonDict = self.readConfig()
+            jsonDict[str(len(jsonDict))] = val
+            with open(self.__filepathfilepath, 'w') as f:
+                json.dump(jsonDict, f, indent=2)
+                return True
+        except:
+            return False

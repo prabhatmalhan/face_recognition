186d8ef0da21dac3bb1f1f7901fdb19a1b5a0961
Prabhat Malhan <prabhat.malhan7@gmail.com>
2022-05-16 06:40:14 +0530

    image preprocessor for prediction

---
+++ model_test/preProcessor.py
@@ -0,0 +1,18 @@
+import cv2
+
+
+def get_face(img):
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+
+    img = cv2.resize(img, (352, 640))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        # roi_gray = cv2.resize(gray[y:y+h, x:x+w], (100, 100))
+        roi_color = cv2.resize(img[y:y+h, x:x+w], (100, 100))
+    try:
+        return (True, roi_color)
+    except:
+        return (False, None)
